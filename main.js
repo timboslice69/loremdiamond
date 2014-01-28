c4bce55b512f7207536e4b3d2cf0ef184a9deb68
timothy.scott <timothy@mediafront.no>
2014-01-28 15:39:51 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,46 @@
+/**
+ * Created by timothy.scott on 1/28/14.
+ */
+
+	var options = {
+		paragraphs: 5,
+		sentances: 6
+	};
+
+
+function randomSentance(){
+	var r = Math.floor((Math.random()*sentances.length)+1);
+	if(typeof(options.lastSentanceIndex) == 'undefined') options.lastSentanceIndex = null;
+	if (r == options.lastSentanceIndex)	return randomSentance();
+	options.lastSentanceIndex = r;
+	return sentances[r];
+}
+
+function submitForm(){
+	generateText(document.getElementById('paragraphs_input').value, document.getElementById('sentances_input').value);
+	return false;
+}
+
+function generateText(paragraphs, sentances){
+	var container = document.getElementById('theText'),
+		html = '',
+		i,
+		j;
+
+	container.innerHTML = '';
+
+	for (i = 0; i < paragraphs; i++){
+		html = html + '<p>';
+		for (j = 0; j < sentances; j++){
+			html = html + randomSentance() + '. ';
+		}
+		html = html + '</p>\n';
+	}
+
+	container.innerHTML = html;
+}
+
+window.onload = function(){
+
+	generateText(options.paragraphs, options.sentances);
+};
